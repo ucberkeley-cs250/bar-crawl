80b6918c6f9c33351651bb6646ae8748375455a3
Sagar Karandikar <karandikarsagar@gmail.com>
2015-11-07 16:41:40 -0800

    update README/scripts to reflect move to fbox instead of a8

--- clusterman/ssh-tunnel-fbox.sh
+++ clusterman/ssh-tunnel-fbox.sh
@@ -2,4 +2,4 @@
 
 # port forward to access the web ui
 # localhost is from the server's perspective, in this case a8 (a8.millennium.berkeley.edu)
-ssh -L 8080:localhost:8080 a8
+ssh -L 8080:localhost:8080 fbox
